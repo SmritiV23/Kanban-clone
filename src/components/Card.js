4e2d57dde115a3363fae17a7e427d14cbd7b8612
SmritiV23 <103731951+SmritiV23@users.noreply.github.com>
2023-06-20 02:08:15 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { FaComment, FaFile } from 'react-icons/fa';
 const Card = ({ title }) => {
   return (
     <div className="bg-white p-4 rounded-lg shadow">
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-gray-500">Card description goes here...</p>
+      <div className='flex justify-end m-2'>
+      <FaComment className="h-4 w-4 mt-2 ml-1 mr-1 text-gray-500" />
+        <span className="text-gray-500 text-sm mt-2 mb-2 ml-1 mr-1">Comments</span>
+        <FaFile className="h-4 w-4 mt-2  ml-1 mr-1 text-gray-500" />
+        <span className="text-gray-500 text-sm mt-2 mb-2 ml-1 mr-1">Files</span>
+      </div>
     </div>
   );
 };
